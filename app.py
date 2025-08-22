049ebbaac47e599bf4c7e8ee0d5e78689d2882d3
get6 <seongjun@ittae.com>
2025-08-22 15:01:57 +0900

    add image

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-from flask import Flask, render_template
+def hello():
+    print("Hello, Add image!")
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html', title='David Flask App')
-
-@app.route('/about')
-def about():
-    return render_template('about.html', title='About')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+hello()
