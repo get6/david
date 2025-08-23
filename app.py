eeb3e7ca4e730ec919d9daf21cceccc62e888f39
get6 <seongjun@ittae.com>
2025-08-23 11:50:45 +0900

    Feat: 메뉴 화면추가
    추가한 기능 설명
    
    Resolves: #1

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-def hello():
-    print("Hello, Add image!")
+# python3 -m pip install --user flask
+from flask import Flask, render_template
 
-hello()
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return '상단에 /menu 를 추가해주세요'
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
