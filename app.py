97778cc3d5f89d64d7a70b67d488cadf9a6e9a86
sunhwang <seongjun@ittae.com>
2025-10-14 16:20:33 +0900

    test2 추가

--- app.py
+++ app.py
@@ -16,5 +16,9 @@ def home():
 def menu():
     return render_template('menu.html')
 
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
